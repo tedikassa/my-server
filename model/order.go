efb332f8793a9cabf00e87a084a331bb98dd5c36
tewodros kassanew <teda16kassa@gmail.com>
2025-09-05 12:23:41 +0300

    update

--- model/order.go
+++ model/order.go
@@ -6,7 +6,7 @@ type Order struct {
     gorm.Model
 		TransactionID string `gorm:"unique"` 
     UserID     uint     `json:"userId" binding:"required"`    
-    User      User         `json:"user,omitempty"` 
+    User      User        `gorm:"foreignKey:UserID" json:"user,omitempty"`
     Status      string       `json:"status" gorm:"default:'pending'" binding:"omitempty,oneof=pending paid delivered cancelled"`
 		
 		TotalPrice float64      `json:"totalPrice" binding:"required,gt=0"`
