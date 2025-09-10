dcac1f6b3f4a9c426bcdeb9e30d5a437d70f1bc4
tewodros kassanew <teda16kassa@gmail.com>
2025-09-10 07:46:55 +0300

    update

--- routes/register_route.go
+++ routes/register_route.go
@@ -2,24 +2,25 @@ package routes
 
 import (
 	"example.com/ecomerce/controller"
+	"example.com/ecomerce/middlewere"
 
 	"github.com/gin-gonic/gin"
 )
 
 
 func RegistorRoutes(server *gin.Engine) {
-  server.POST("/api/product",controller.AddProduct)
-	server.POST("/api/payment/:id",controller.Payment)
-   server.GET("/api/product",controller.GetAllProduct)
-	server.GET("/api/product/:id",controller.GetProductById)
+  server.POST("/api/product",middlewere.AuthMiddlewere,controller.AddProduct)
+	server.POST("/api/payment/:id",middlewere.AuthMiddlewere,controller.Payment)
+   server.GET("/api/product",middlewere.AuthMiddlewere,controller.GetAllProduct)
+	server.GET("/api/product/:id",middlewere.AuthMiddlewere,controller.GetProductById)
 	server.POST("/api/signup",controller.Signup)
 	server.POST("/api/login",controller.Login)
-	server.PATCH("/api/update/user/:id",controller.UpdateUser)
+	server.PATCH("/api/update/user/:id",middlewere.AuthMiddlewere,controller.UpdateUser)
 	server.POST("/api/webhook/incoming", controller.SantimpayWebhookIncoming)
-	server.POST("/api/merchant/delivery/:id",controller.ConfirmDelivery)
-	server.GET("/api/orders",controller.GetAllOrder)
-	server.GET("/api/merchant/product/:id",controller.GetMerchantProduct)
-	server.GET("/api/merchant/order/:id",controller.MerchantOrder)
+	server.POST("/api/merchant/delivery/:id",middlewere.AuthMiddlewere,controller.ConfirmDelivery)
+	server.GET("/api/orders",middlewere.AuthMiddlewere,controller.GetAllOrder)
+	server.GET("/api/merchant/product/:id",middlewere.AuthMiddlewere,controller.GetMerchantProduct)
+	server.GET("/api/merchant/order/:id",middlewere.AuthMiddlewere,controller.MerchantOrder)
 	server.POST("/api/webhook/payout",controller.SantimpayWebhookPayout)
-	server.POST("/api/merchant/payment/:id",controller.AskPayout)
+	server.POST("/api/merchant/payment/:id",middlewere.AuthMiddlewere,controller.AskPayout)
 }
