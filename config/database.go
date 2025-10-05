83d57182e6bcfae56191eb4a439bb1079242d9e3
tewodros kassanew <teda16kassa@gmail.com>
2025-10-05 10:05:29 +0300

    update

--- config/database.go
+++ config/database.go
@@ -1,23 +1,35 @@
 package config
 
 import (
+	"fmt"
+	"log"
+
 	"example.com/ecomerce/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
- func ConnectDatabase()  {
-	var err error
-	dsn:="host=dpg-d2svpvbe5dus73db7f20-a user=ecomerce_3rsi_user password=N4iWNLISUHvesVIbjIa49ogVvOUr263R dbname=ecomerce_3rsi port=5432 sslmode=disable"
-	DB,err=gorm.Open(postgres.Open(dsn),&gorm.Config{})
-	if err!=nil {
-		panic("server coudnt connect connect")
-	}
+func ConnectDatabase() {
+    dsn := "host=dpg-d2svpvbe5dus73db7f20-a.oregon-postgres.render.com user=ecomerce_3rsi_user password=N4iWNLISUHvesVIbjIa49ogVvOUr263R dbname=ecomerce_3rsi port=5432 sslmode=require"
+    
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("❌ Failed to connect to database: %v", err)
+    }
 
- println("Database connection establish")
- DB.AutoMigrate(&model.Product{},&model.User{},&model.Image{},&model.MerchantProfile{},&model.Order{},&model.OrderItem{})
- }
+    DB = db
+    fmt.Println("✅ Database connection established")
+
+    db.AutoMigrate(
+        &model.Product{},
+        &model.User{},
+        &model.Image{},
+        &model.MerchantProfile{},
+        &model.Order{},
+        &model.OrderItem{},
+    )
+}
  func ResetDatabase() {
 	
 	DB.Migrator().DropTable(
